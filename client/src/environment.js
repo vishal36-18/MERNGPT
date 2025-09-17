deb8230b3fe371394c9ec7207053f291f95c3d4f
vishal36-18 <vishaltjttt@gmail.com>
2025-09-17 20:12:04 +0000

    server updated

--- client/src/environment.js
+++
@@ -1,8 +0,0 @@
-let IS_PROD = false;
-const server = IS_PROD ?
-    "https://merngptserver.onrender.com/" :
-
-    "http://localhost:5050"
-
-
-export default server;
--- client/src/server.jsx
+++ client/src/server.jsx
@@ -1,4 +1,4 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
     "https://merngptserver.onrender.com" :
 
